8e7b515ead08a62b2704eaa3cd877c7cfdb20114
Franconobile <eifrann22@gmail.com>
2021-12-05 19:38:56 -0300

    Actualizacion con responsive

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-window.onscroll = function(){miFuncion()};
+$(window).scroll( function(){
+    $('.gaming-info').each( function(i){
 
-var 
+        var bottom_of_object =  $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        if( bottom_of_window > bottom_of_object - 400 ){
+            $(this).addClass('is-visible');
+        }
+        else {
+            $(this).removeClass('is-visible');
+        }
+    });
+});
